5fd60ddd9b27ad7ae40d67234a15a93715adca5b
Zach Montgomery <zachary.s.montgomery@gmail.com>
2021-07-01 14:34:22 -0600

    complete overhaul using classes and allowing multiprocessing

--- polyFits/__init__.py
+++ polyFits/__init__.py
@@ -1,3 +1,3 @@
-from .polyFits import multivariablePolynomialFit, multivariablePolynomialFunction, multivariableR2, multivariableRMS, compose_j, decompose_j, calcJ, kDecompose, kCompose, autoPolyFit, list2dict, dict2list, polyFit2json, zachsAutoPolyFit
+from .polyFits import database, polyFit
 
 ## oneLineProgress, multiSort, isClose, zachsAutoPolyFit, 
